83bb6e0a41ef5be9074f980822de2967fc85c167
Paul Padamadan <pauljoy06@gmail.com>
2025-11-28 16:10:07 +0530

    feat: add Cors support

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,13 @@
 import 'dotenv/config';
 import express from 'express';
+import cors from 'cors';
 import userRoutes from './routes/user.routes.js';
 import errorHandler from './middleware/errorHandler.js';
 import AppError from './utils/AppError.js';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/users', userRoutes);
